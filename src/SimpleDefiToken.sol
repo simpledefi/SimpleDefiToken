c44d6925adcb14a12ddf220acd15798c92fe1713
Derrick Bradbury <derrickb@halex.com>
2023-01-26 16:20:07 -0500

    Updated contract and testing

--- src/SimpleDefiToken.sol
+++ src/SimpleDefiToken.sol
@@ -10,9 +10,33 @@ contract EasyToken is ERC20Capped, ERC20Burnable, ERC20Snapshot, Ownable {
         uint256 amount;
     }
 
+    mapping(address=>uint) public releaseAddresses;
+    bool public locked;
+    uint public releaseBlock;
+
     event MintRelease(address indexed to, uint256 value);
+    event releaseAddressAdd(address _addr, uint _blockNumber);
     event SnapshotMade(uint id);
-    constructor() ERC20("SimpleDEFI", "EASY") ERC20Capped(400 * 1e24) {}
+    event TokenReleased();
+
+    error functionLocked();
+    error invalidBlockNumber();
+    constructor(uint _releaseBlock) ERC20("SimpleDEFI", "EASY") ERC20Capped(400 * 1e24) {
+        releaseBlock = _releaseBlock;
+        locked = true;
+    }
+
+    function addRelease(address _addr, uint _blockNumber) external onlyOwner {
+        uint _rd = releaseAddresses[_addr];
+        if (_rd > 0 && _blockNumber > _rd) revert invalidBlockNumber();
+        releaseAddresses[_addr] = _blockNumber;
+        emit releaseAddressAdd(_addr, _blockNumber);
+    }
+
+    function releaseToken() external onlyOwner {
+        locked = false;
+        emit TokenReleased();
+    }
 
     function mint(mintTo[] calldata _mintTo) external onlyOwner{
         uint subtotal;
@@ -34,6 +58,7 @@ contract EasyToken is ERC20Capped, ERC20Burnable, ERC20Snapshot, Ownable {
 
     function _beforeTokenTransfer(address _from, address _to, uint256 _amount) internal override(ERC20, ERC20Snapshot)
     {
+        if (locked == true && block.number <= releaseBlock && (releaseAddresses[msg.sender] == 0 || block.number <= releaseAddresses[msg.sender]) && msg.sender != owner()) revert functionLocked();        
         super._beforeTokenTransfer(_from, _to, _amount);
     }
 
